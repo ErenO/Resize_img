59f67f6f7fcde9b693c73bb1fdf6e2ec7e085d69
Eren Ozdek <eren_ozdek@Erens-MacBook-Pro.local>
2018-02-12 04:01:37 +0100

    image resizer

---
+++ resize_img.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+from PIL import Image
+import os, sys
+
+path = "/Users/eren_ozdek/Desktop/ART42test/train/"
+dirs = os.listdir( path )
+
+def resize():
+    for item in dirs:
+        print (item)
+        if os.path.isfile(path+item):
+            print (path+item)
+            im = Image.open(path+item)
+            f, e = os.path.splitext(path+item)
+            imResize = im.resize((200,200), Image.ANTIALIAS)
+            imResize.save(f + '.jpg', 'JPEG', quality=90)
+
+resize()
+print ("hello")
